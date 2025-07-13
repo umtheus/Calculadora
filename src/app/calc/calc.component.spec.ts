a4d1b596932ebbeea02be31acb29cbee60dd913e
umtheus <gm.arr.matheus@gmail.com>
2025-07-12 22:19:35 -0300

    test(CalcComponent):testa componente

--- src/app/calc/calc.component.spec.ts
+++ src/app/calc/calc.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalcComponent } from './calc.component';
+import { By } from '@angular/platform-browser';
 
 describe('CalcComponent', () => {
   let component: CalcComponent;
@@ -15,7 +16,44 @@ describe('CalcComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve adicionar operador apos um numero valido', () => {
+    component.display = '2';
+    component.append('+');
+    expect(component.display).toBe('2+');
+  });
+  it('deve calcular expressoes validas', () => {
+    component.display = '2';
+    component.append('+');
+    component.append('2');
+    component.calculate();
+    expect(component.display).toBe('4');
+  });
+  it('deve limpar o display', () => {
+    component.display = '2';
+    component.clear();
+    expect(component.display).toBe('0');
+  });
+
+  it('deve retornar erro para expressao invalida', () => {
+    component.display = '5++5';
+    component.calculate();
+    expect(component.display).toBe('Error');
+  });
+  it('deve excluir ultimo caractere', () => {
+    component.display = '123';
+    component.del();
+    expect(component.display).toBe('12');
+  });
+  it('deve atualizar display ao clicar no botao 1', () => {
+    const botao1 = fixture.debugElement
+      .queryAll(By.css('button'))
+      .find((el) => el.nativeElement.textContent.trim() === '1');
+
+    expect(botao1).toBeTruthy();
+
+    botao1?.nativeElement.click();
+    fixture.detectChanges(); //vai atualizar o a tela do component depois de clicar
+
+    expect(component.display).toBe('1');
   });
 });
