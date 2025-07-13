2eaf58d959e82a56ae7a1c27cf9121fc07a5a0fd
umtheus <gm.arr.matheus@gmail.com>
2025-07-12 22:17:01 -0300

    feat(AppComponent):remove teste h1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,15 +8,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome calculator'
-    );
-  });
-
   it(`should have as title 'calculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
