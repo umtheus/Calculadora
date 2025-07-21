b35d198fda04e5be4884b132d17ac6534167f2ea
umtheus <gm.arr.matheus@gmail.com>
2025-07-21 00:27:14 -0300

    feat(keypadComponente): cria keypad componente

---
+++ src/app/calculator/keypad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-keypad',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './keypad.component.html',
+  styleUrl: './keypad.component.scss',
+})
+export class KeypadComponent {}
