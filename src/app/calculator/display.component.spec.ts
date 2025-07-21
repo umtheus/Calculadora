b74ad268bc7848cf9b92a34a37794fee82008808
umtheus <gm.arr.matheus@gmail.com>
2025-07-21 03:44:25 -0300

    refactor: atualiza lógica da calculadora

--- src/app/calculator/display.component.spec.ts
+++ src/app/calculator/display.component.spec.ts
@@ -15,7 +15,35 @@ describe('DisplayComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve atualizar o display ao inserir um numero valido', () => {
+    component.atualizarDisplay('7');
+    expect(component.display).toBe('7');
+  });
+  it('deve iniciar com 0', () => {
+    expect(component.display).toBe('0');
+  });
+  it('nao deve permetir operadores duplicados', () => {
+    component.atualizarDisplay('1');
+    component.atualizarDisplay('+');
+    component.atualizarDisplay('+');
+    expect(component.display).toBe('1+');
+  });
+  it('deve limpar o display', () => {
+    component.atualizarDisplay('1');
+    component.atualizarDisplay('+');
+    component.atualizarDisplay('C');
+    expect(component.display).toBe('0');
+  });
+  it('deve apagar o ultimo caractere ao precionar o AC', () => {
+    component.atualizarDisplay('2');
+    component.atualizarDisplay('-');
+    component.atualizarDisplay('AC');
+    expect(component.display).toBe('2');
+  });
+
+  it('deve aplicar porcentagem corretamente', () => {
+    component.atualizarDisplay('10');
+    component.atualizarDisplay('%');
+    expect(component.display).toBe('0.1');
   });
 });
