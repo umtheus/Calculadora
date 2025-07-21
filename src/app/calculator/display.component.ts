9b9f30c31084c95c596d57d568f0229c88f4f5de
umtheus <gm.arr.matheus@gmail.com>
2025-07-21 00:27:48 -0300

    feat(DisplayComponente): cria display componente

---
+++ src/app/calculator/display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-display',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './display.component.html',
+  styleUrl: './display.component.scss',
+})
+export class DisplayComponent {}
